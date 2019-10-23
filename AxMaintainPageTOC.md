0cfc2aebdee985a135c50e05a2c1aaa7a2b398b0
wthaem <k-w.thaemelt@vodafone.de>
2019-10-23 12:35:18 +0200

    add script for managing TOC's on Wiki pages and its docu

---
+++ AxMaintainPageTOC.md
@@ -0,0 +1,110 @@
+[Prev](AxMaintainIndex) (Maintaining the QMS Wiki Index) | [Home](Home) | [Manual](DocMain) | [Index](AxAdvIndex) | (Playground - start your new Wiki page) [Next](DocPlayground)
+- - -
+
+***Table of contents***
+
+* [Manage Table Of Content on Wiki pages](#manage-table-of-content-on-wiki-pages)
+    * [The `AddToc.py` script](#the-addtocpy-script)
+        * [Purpose of the script](#purpose-of-the-script)
+        * [Running the script](#running-the-script)
+        * [Using the script in a client-side hook](#using-the-script-in-a-client-side-hook)
+        * [Limitations of the script](#limitations-of-the-script)
+        * [Used anchor building (slugify) method](#used-anchor-building-slugify-method)
+    * [Differences in rendering TOC's ](#differences-in-rendering-tocs)
+
+* * * * * * * * * *
+ 
+
+# Manage Table Of Content on Wiki pages
+
+The Markdown language for Bitbucket supported a `[TOC]` macro which created and updated automatically a table of content (**TOC**) on each Wiki page.
+
+There is no known way to do the same with GitHub Markdown.
+
+For obvious reasons, GitHub doesn't allow to execute arbitrary code on their servers as part of server-side Git hooks. 
+
+Thus, the easiest way to create and update TOC's on Wiki pages is to use a client-side script on a local copy of the remote Wiki. This approach is similar to the handling of navigation bars for Wiki pages and it has the same disadvantages:
+
+* If a Wiki page is edited on the server, then the TOC may get out of sync.
+* If a Wiki page is edited locally, then the user *should* run a script to handle necessary updates of the pages TOC. This can be automated with a client-side Git hook, but this hook needs special manual installation by each local user.
+
+## The `AddToc.py` script
+
+### Purpose of the script
+
+This script finds all pages/MD files in the Wiki and adds or updates the table of contents to those files which have a navigation bar. There is a list of MD files (exception list) that don't get a TOC.
+
+### Running the script
+
+* Ensure Python is installed on your computer (no additional modules required).
+* Run first `git pull` before locally editing Wiki pages or running the script so that the Wiki on the server and its local copy are in sync.
+* After finishing with edit open a command window, change to the `Tools` subdirectory of the local copy of the Wiki and run the script `AddTOC.py` located in this subdirectory.
+* `stdout` (output to the command window) informs about actions carried out by the script on the Wiki pages. There are 3 possible types of output messages:
+    * `Page left without TOC:  ..\file.md`   - no TOC added to this file, file is in special exclusion list
+    * `TOC on page ok.         ..\file.md`   - file has valid TOC, no change to the file
+    * `TOC updated:            ..\file.md`   - file has been changed and has got updated (or new) TOC
+
+
+### Using the script in a client-side hook
+
+The script can be started automatically during a `git commit` when defining a client-side hook. To configure the hook put a file with the name `pre-commit` into the `hook` subdirectory of the local Wiki copy with the following content:
+
+~~~
+python ./Tools/AddToc.py
+git add -u 
+exit 0
+~~~
+
+**Prerequisite:** Python must be installed.
+
+**Warning:** The hook script always exits with exit code 0. If by some reason `AddToc.py` fails to run as expected, then the user should - if necessary - remove the change made by `git add -u`.
+
+### Limitations of the script
+
+The following limitations hold true for the script and the handled MD files:
+
+* Supported Wiki languages are English, German, Spanish, and Russian. If other languages than the mentioned ones are included into the Wiki, then the list of non-ASCII alphabetic characters has to be updated in the the script.
+* Filenames in the complete Wiki including QMapTool should be unique
+* Restrictions for header lines in Wiki pages:
+    * a header line should have its "#" prefixes starting in column 1
+    * header lines should be unique within a Wiki page
+    * header lines should be on 1 line
+* Supported and tested header line formats include the following ones:
+    * `C1.) Compile the GDAL library, http://www.gdal.org/` (special characters and link)
+    * `The _Markdown_ Language` (italics)
+    * `The ``markdown`` language in action` (code)
+    * `![EditDetails](images/DocGisItemsWpt/EditDetails.png) View / Edit Details` (image link)  
+     *Remark:* If `EditDetails` appears in front of `View / Edit Details`, then referenced image file is not found and instead of the image there appears the supplied image title.
+    * `Only lowercase German non-ASCII: Lüdenscheid` (works with Google Chrome browser, too)
+    * `Spanish and German characters: Äáäéíñóöúü` (works in GitHub, not with Chrome)
+    * `Загрузка файлов из Интернета`  (doesn't work with Chrome)
+    * `Cyrillic text Mixed with Other text: Загрузка файлов из Интернета` (doesn't work with Chrome)
+* The TOC in a Wiki page starts with the special line `***Table of contents***`
+* The TOC in a Wiki page ends with the special line `* * * * * * * * * *`
+* The TOC in a Wiki page is located immediately after the navbar on top of the Wiki page
+* The TOC shouldn't be changed manually
+* A TOC is inserted into a Wiki page only 
+    * if there is a navbar on top of the page and
+    * if the file is not included in the special exclusion list of the script.
+
+### Used anchor building (slugify) method
+
+For handling non-ASCII header lines in GitHub correctly the usual method for anchor/link building (the so-called slugify method) was modified. It consists of the following essential steps:
+
+* Open MD file and find all header lines (lines starting with 1 or more "#" characters).
+* If there is an image link in the header line, replace it with "-".
+* Remove all characters not in the list `0-9a-zа-яßáäéíñóöúü\s-` (list consists of all digits, alphabetic characters of used languages, whitespace and "-").
+* Convert all ASCII characters to lowercase.
+* If uppercase non-ASCII characters are in the remaining string then add the prefix `user-content-` to the string.
+* Replace whitespace with "-".
+
+
+
+## Differences in rendering TOC's 
+
+Markdown files (Wiki pages) can be rendered using various rendering engines: GitHub, BitBucket, Google Chrome with markdown extension, ...
+
+These rendering engines are using different rules for link/anchor building in the case of non-ASCII header lines. The given script creates TOC's for rendering in GitHub. TOC links for header lines with non-ASCII characters might be broken when using different rendering engines. 
+
+- - -
+[Prev](AxMaintainIndex) (Maintaining the QMS Wiki Index) | [Home](Home) | [Manual](DocMain) | [Index](AxAdvIndex) | [Top](#) | (Playground - start your new Wiki page) [Next](DocPlayground)
---
+++ Tools/AddTOC.py
@@ -0,0 +1,225 @@
+
+# Add or update TOC to all Wiki pages
+
+# Author: W. Thämelt, 23.10.2019
+
+################################################################
+
+# Assumptions & restrictions:
+# ---------------------------
+
+# - a TOC is added only to pages with a navbar
+# - a header line should have its "#" prefixes starting in column 1
+# - header lines should be unique within a Wiki page
+# - header lines should be on 1 line
+# - the TOC starts with the special line "***Table of contents***"
+# - the TOC ends with the special line "* * * * * * * * * *"
+# - the TOC should be located immediately after the navbar
+# - filenames in the complete Wiki including QMapTool should be unique
+#
+# - There seems to be issues in Github Wiki when handling anchors for non-ASCII languages.
+#   Proof: open TestAnchors.md in GitHub Wiki, move to Russian header, select link and
+#          open in new tab ===> fails. ASCII headers don>'t fail!
+#   Therefore, only pages with English text get a TOC.
+#
+# - There are differences in anchor building between different markdown renderers.
+#   This holds true for headers without strange formats.
+#   Thus, everything must be checked in the browser with the Github Wiki.
+# - In a local copy of the Wiki local links may fail due to differences in the
+#   anchor constructions rules used. This happens if in a header occur uppercase
+#   non-ASCII letters (more exactly: German, Spanish, or Russian ones).
+# - If other languages than the mentioned ones are included in the Wiki, then the list of respective
+#   non-ASCII characters has to be updated.
+
+################################################################
+
+import os
+import re
+import unicodedata
+import fnmatch
+
+rheader = re.compile(r"(#+)\s*([^#]+)")       # mask for MD header line
+
+# mask for finding and deleting image links in section headers
+# matches:   ## ![EditDetails](images/DocGisItemsWpt/EditDetails.png) View / Edit Details
+r4 = re.compile(r"\!\[[^]]*\]\([^)]*\)\s+")   # include spaces at the end of the link
+r1 = re.compile("[A-Z]+")                     # matches ASCII uppercase
+r2 = re.compile("[А-ЯÁÄÉÍÑÓÖÚÜ]")             # matches non-ASCII uppercase - don't change them
+
+WIKIDIR = ".."                               # script is located in Tools subdirectory!
+filemask = "*.md"
+
+MAXHDRLVL = 6                                 # maximum header level used in TOC
+
+# no TOC inserted into the following pages:
+MDWithoutTOC = ("DocMain.md", "AxAdvIndex.md", "AxAdvToc.md", "Home.md", "AxAdvTocAbbr.md",
+                "AxGlossary.md", "AxHotkeys.md", "AxWikiImages.md",
+                "QMTDocMain.md", "QMTAxAdvIndex.md", "QMTAxAdvToc.md", "QMTHome.md",
+                )
+
+MDWithoutTOC = tuple(r"%s" % x.lower() for x in MDWithoutTOC) # necessary for comparisons
+
+################################################################
+
+# find each MD file
+def gen_find(filepat, top):
+    # filepat: pattern of files to find
+    # top: folder where search for files is started
+
+    for path, dirlist, filelist in os.walk(top):
+        for name in fnmatch.filter(filelist, filepat):
+            yield os.path.join(path, name)
+
+################################################################
+
+# build MD reference from given string using separator as word separator
+
+# Github version working with non-ASCII   
+def f(matchobj):                               # convert ASCII uppercase to lowercase
+    return matchobj.group(0).lower()
+    
+def slugify1(value, separator):  # variant handling non-ASCII in GitHub correctly
+    value = re.sub(r'[^0-9a-zа-яßáäéíñóöúü\s-]', '', value, flags=re.IGNORECASE).rstrip() # drop non-alphabetic characters
+    value = r1.sub(f, value)                    # make only ASCII uppercase to lower
+  
+    if r2.search(value):                       # non-ASCII uppercase found
+        value = "user-content-%s" % value      # use "id" instead of "href" for link target
+  
+    return re.sub(r'\s', separator, value).strip() # replace space with separator ("-")
+  
+################################################################
+def FindTOC4Page(mdfile):                          # find existing TOC in file
+
+    if os.path.basename(mdfile).lower() in MDWithoutTOC: # these files are left without TOC
+        return "PageWithoutTOC"
+        
+    inpf = open(mdfile, "r", encoding="utf-8")
+
+    toc = []
+
+    lines = inpf.readlines()
+    
+    if (lines[0].startswith("[Prev]") or lines[0].startswith("Prev")) and \
+        lines[1].startswith("- - -"):                     # page has navbar        
+
+        tocfound = False
+        firstidx = False                                  # index of TOC header line
+        lastidx = False                                   # index of TOC footer line
+        
+        for i, lne in enumerate(lines[2:]):
+            if lne[:-1] == "***Table of contents***":     # TOC header found
+                tocfound = True
+                toc.append(lne[:-1])
+                firstidx = i+2                            # save index of header line for later update
+                
+            elif tocfound and lne[:-1] != "* * * * * * * * * *": # normal TOC line found
+                toc.append(lne[:-1])
+                
+            elif lne[:-1] == "* * * * * * * * * *":       # TOC footer found
+                toc.append(lne[:-1])
+                lastidx = i+3                             # save index of footer line for later update
+                return [toc, firstidx, lastidx]
+                
+            else:                                         # line not related to TOC
+                continue
+                
+        return [toc, firstidx, lastidx]                   # existing TOC with index for header and footer lines
+                   
+    else:                                                 # pages without navbar don't get TOC (e.g. _sidebar.md)
+        return "PageWithoutTOC"
+
+################################################################
+def BuildTOC4Page(mdfile):                        # build actual TOC for given file
+    inpf = open(mdfile, "r", encoding="utf-8")
+
+    toc = ["***Table of contents***", "", ]       # TOC header
+
+    for lne in inpf:                              # try to find TOC in file
+
+        rr = rheader.match(lne)
+   
+        if rr:                                    # TOC header found in file
+        
+            hdr = rr.group(2)                     # header text
+            hdr = r4.sub("-", hdr)                # remove possible image link
+
+            anchor = slugify1(hdr, "-")            # build Github anchor
+            spacer = "    " * (len(rr.group(1)) - 1) # find necessary indentation for TOC output list
+            
+            if hdr.startswith("-"):               # drop superfluous "-" as first character
+                hdr = hdr[1:]
+
+            if MAXHDRLVL >= len(rr.group(1)) - 1: # avoid too may header levels
+                toc.append("%s* [%s](#%s)" % (spacer, hdr[:-1], anchor)) # add 1 TOC line  
+        
+    toc.append("")    
+    toc.append("* * * * * * * * * *")             # add footer to TOC
+    
+    inpf.close()
+    
+    return toc                                    # used for comparison with existing TOC in file
+
+################################################################
+def UpdateTOC4Page(mdfile, toc0, toc):                  # insert or update TOC in file
+
+    inpf = open(mdfile, "r", encoding="utf-8")
+    lines = inpf.readlines()                            # read complete file
+    inpf.close()
+    
+    if toc0[1:] == [False, False]:                      # file without TOC
+    
+        toc0[1:] = [2, 3]                                # split lines in file here
+       
+        lines[toc0[1]:toc0[2]] = [" \n"] + ["%s\n" % x for x in toc] + [" \n"] # insert new TOC at proper location and add newlines
+       
+    else:                                               # file with TOC
+        lines[toc0[1]:toc0[2]] = ["%s\n" % x for x in toc] # replace TOC with updated one
+
+    outf = open(mdfile, "w", encoding="utf-8")        # output of updated page - old one is overwritten!
+    
+    for lne in lines:
+        outf.write(lne)
+        
+    outf.close()
+    return
+    
+################################################################
+def HandlePage(mdfile):                                  # decide how to handle TOC in file
+
+    toc0 = FindTOC4Page(mdfile)                          # check if TOC already exists in file
+
+    if toc0 == "PageWithoutTOC":                         # page doesn't need a TOC
+        print("Page left without TOC: ", mdfile)
+        
+    elif toc0[1:] == [False, False]:                     # page does not yet have a TOC
+        print("Page with missing TOC:", mdfile)
+        
+        toc = BuildTOC4Page(mdfile)                      # build necessary TOC for page
+        
+        UpdateTOC4Page(mdfile, toc0, toc)                # and insert it into page
+        
+    else:                                                # page has TOC
+
+        toc = BuildTOC4Page(mdfile)                      # build actual TOC for page
+ 
+        if toc == toc0[0]:                               # compare actual with existing TOC
+            print("TOC on page ok.        ", mdfile)     # if equal - nothing to do
+            
+        else:                                            # existing TOC needs update - do it
+            print("TOC updated:           ", mdfile)
+            UpdateTOC4Page(mdfile, toc0, toc)    
+
+    return
+      
+################################################################      
+
+if __name__ == '__main__':
+
+    mdfiles = gen_find(filemask, WIKIDIR)             # find all *.md files
+
+    for mdfile in mdfiles:                            # insert or update TOC in all MD files
+        HandlePage(mdfile)
+    
+    print("\nEnd of run.")
+
+    
